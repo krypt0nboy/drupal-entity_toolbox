ee22e4dedf0378168bcdbeaaea3beda7f0faaa81
kryptonboy <kryptonb0y@harold-cohen.com>
2016-09-19 01:28:24 +0200

    Module update

--- js/entity_toolbox.tree.js
+++ js/entity_toolbox.tree.js
@@ -2,6 +2,4 @@
     Drupal.entitytoolbox = Drupal.entitytoolbox || {};
     Drupal.entitytoolbox.behaviors = Drupal.entitytoolbox.behaviors || {};
     Drupal.entitytoolbox.hashes = Drupal.entitytoolbox.hashes || {};
-
-
 })(jQuery);
